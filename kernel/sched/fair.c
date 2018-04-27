d475d97712fe3529cfbab1ad9bce7511b87021b3
Ionela Voinescu <ionela.voinescu@arm.com>
2018-04-27 18:19:13 +0100

    ANDROID: sched/fair: unify spare capacity calculation
    Given that we have a few sites where the spare capacity of a CPU is
    calculated as the difference between the original capacity of the CPU
    and its computed new utilization, let's unify the calculation and use
    that value tracked with a local spare_cap variable.
    
    Change-Id: I78daece7543f78d4f74edbee5e9ceb62908af507
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7026,6 +7026,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long capacity_curr = capacity_curr_of(i);
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util, new_util_cuml;
+			long spare_cap;
 
 			trace_sched_cpu_util(i);
 			if (!cpu_online(i) || cpu_isolated(i))
@@ -7071,6 +7072,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 			if (cpu_check_overutil_condition(i, new_util))
 				continue;
+
+			/*
+			 * Pre-compute the maximum possible capacity we expect
+			 * to have available on this CPU once the task is
+			 * enqueued here.
+			 */
+			spare_cap = capacity_orig - new_util;
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7123,9 +7131,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
-				if ((capacity_curr > new_util) &&
-					(capacity_orig - new_util > target_max_spare_cap)) {
-					target_max_spare_cap = capacity_orig - new_util;
+				if (capacity_curr > new_util &&
+				    spare_cap > target_max_spare_cap) {
+					target_max_spare_cap = spare_cap;
 					target_cpu = i;
 					continue;
 				}
@@ -7247,10 +7255,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 			/* Favor CPUs with maximum spare capacity */
 			if (capacity_orig == target_capacity &&
-			    (capacity_orig - new_util) < target_max_spare_cap)
+			    spare_cap < target_max_spare_cap)
 				continue;
 
-			target_max_spare_cap = capacity_orig - new_util;
+			target_max_spare_cap = spare_cap;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
