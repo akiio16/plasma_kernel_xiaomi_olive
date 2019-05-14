36b309c4ec503bc6619784f75c0a4b1546c31c53
0ctobot <return.of.octobot@gmail.com>
2019-05-13 22:03:09 -0400

    cpu_input_boost: Allow configuration of LP cluster's minimum idle frequency
    Subsequent to:
    447dc35 ("cpu_input_boost: Remove frame boosting entirely [1/2]")
    6838cc8 ("drm/atomic: Remove frame boosting entirely [2/2]")
    
    In lieu of maintaining a seperate boost level for rendering, we
    will be globally raising the active minimum frequency for LP
    cluster to 748MHz. In order to mitigate any negative impact this
    may have on idle drain, allow for secondary configuration of LP's
    screen-off minimum.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -31,6 +31,8 @@ static unsigned int frame_boost_freq_lp __read_mostly =
 	CONFIG_FRAME_BOOST_FREQ_LP;
 static unsigned int frame_boost_freq_hp __read_mostly =
 	CONFIG_FRAME_BOOST_FREQ_PERF;
+static unsigned int idle_min_freq_lp __read_mostly =
+	CONFIG_IDLE_MIN_FREQ_LP;
 static unsigned int remove_input_boost_freq_lp __read_mostly =
 	CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
 static unsigned int remove_input_boost_freq_perf __read_mostly =
@@ -47,6 +49,7 @@ module_param(max_boost_freq_lp, uint, 0644);
 module_param(max_boost_freq_hp, uint, 0644);
 module_param(frame_boost_freq_lp, uint, 0644);
 module_param(frame_boost_freq_hp, uint, 0644);
+module_param(idle_min_freq_lp, uint, 0644);
 module_param(remove_input_boost_freq_lp, uint, 0644);
 module_param(remove_input_boost_freq_perf, uint, 0644);
 
@@ -143,9 +146,13 @@ static unsigned int get_frame_boost_freq(struct cpufreq_policy *policy)
 
 static unsigned int get_min_freq(struct cpufreq_policy *policy)
 {
+	struct boost_drv *b = &boost_drv_g;
 	unsigned int freq;
 
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask) &&
+			test_bit(SCREEN_OFF, &b->state))
+		freq = idle_min_freq_lp;
+	else if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = remove_input_boost_freq_lp;
 	else
 		freq = remove_input_boost_freq_perf;
